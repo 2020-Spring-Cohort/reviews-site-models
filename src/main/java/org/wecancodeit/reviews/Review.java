c2ea87b348d7ae1b6a3bcd13962767eb3d40d2b2
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-14 15:04:06 -0500

    Refactored Review
    Got rid of color and size for Review POJO to make it more flexible.
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -5,15 +5,11 @@ public class Review {
     private String name;
     private String description;
     private int price;
-    private String color;
-    private double size;
 
-    public Review(String name, String description, int price, String color, double size) {
+    public Review(String name, String description, int price) {
         this.name = name;
         this.description = description;
         this.price = price;
-        this.color = color;
-        this.size = size;
     }
 
     public String getName() {
@@ -26,13 +22,5 @@ public class Review {
 
     public int getPrice() {
         return price;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public double getSize() {
-        return size;
     }
 }
