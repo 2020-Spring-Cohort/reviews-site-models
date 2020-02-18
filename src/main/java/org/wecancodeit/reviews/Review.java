3b046460903bbaae1530338292d72772a3b25b33
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-18 10:43:02 -0500

    Review POJO Refactor
    Added a static count for reviews, added id field and getter. Assigned id to incremented count in constructor
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -5,11 +5,15 @@ public class Review {
     private String name;
     private String description;
     private int price;
+    private static int idCount = 1;
+    private int reviewId;
 
     public Review(String name, String description, int price) {
         this.name = name;
         this.description = description;
         this.price = price;
+        idCount++;
+        this.reviewId = idCount;
     }
 
     public String getName() {
@@ -22,5 +26,9 @@ public class Review {
 
     public int getPrice() {
         return price;
+    }
+
+    public int getReviewId() {
+        return reviewId;
     }
 }
