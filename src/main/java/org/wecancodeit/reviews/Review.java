61a1c7986d2396a6a3fda8ecfe0d4d3aa46b4aa4
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-19 14:38:48 -0500

    Model Fix
    Fixed model attributes so Thymeleaf can provide autocompletions and linting
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -2,21 +2,35 @@ package org.wecancodeit.reviews;
 
 public class Review {
 
-    private String name;
+    private String reviewTitle;
+    private String userName;
     private String description;
     private int price;
     private static int idCount = 1;
     private int reviewId;
 
-    public Review(String name, String description, int price) {
-        this.name = name;
+    public Review(String reviewTitle, String description, int price) {
+        this.reviewTitle = reviewTitle;
+        this.userName = "Anonymous";
         this.description = description;
         this.price = price;
-        this.reviewId = ++idCount;
+        this.reviewId = idCount++;
     }
 
-    public String getName() {
-        return name;
+    public Review(String reviewTitle, String userName, String description, int price) {
+        this.reviewTitle = reviewTitle;
+        this.userName = userName;
+        this.description = description;
+        this.price = price;
+        this.reviewId = idCount++;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getReviewTitle() {
+        return reviewTitle;
     }
 
     public String getDescription() {
--- src/main/java/org/wecancodeit/reviews/controllers/CategoriesController.java
+++ src/main/java/org/wecancodeit/reviews/controllers/CategoriesController.java
@@ -3,14 +3,17 @@ package org.wecancodeit.reviews.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wecancodeit.reviews.models.CategoryStorage;
 
 @Controller
 @RequestMapping("categories")
 public class CategoriesController {
 
+    CategoryStorage storage;
+
     @RequestMapping(value = "")
     public String displayCategories(Model model) {
-        model.addAttribute("title", "categories");
+        model.addAttribute("categories", storage.getAll());
         return "categories";
     }
 
