22c9d651b40913f17d454da98f4c69dd0c5714db
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-21 10:23:59 -0500

    Controller test suites
    Tests made for ReviewController and CategoriesController, still need to do HashtagController tests but Hashtag still needs implemented throughout the app
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

---
+++ src/test/java/org/wecancodeit/reviews/controllers/ReviewControllerTest.java
@@ -0,0 +1,57 @@
+package org.wecancodeit.reviews.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.wecancodeit.reviews.models.Category;
+import org.wecancodeit.reviews.models.Review;
+import org.wecancodeit.reviews.storage.ReviewStorage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ReviewControllerTest {
+
+    private ReviewController underTest;
+    private Model model;
+    private ReviewStorage mockStorage;
+    private Review testReview;
+
+    @BeforeEach
+    void setUp() {
+        mockStorage = mock(ReviewStorage.class);
+        underTest = new ReviewController(mockStorage);
+        model = mock(Model.class);
+        Category testCategory = new Category("Good");
+        testReview = new Review(testCategory, "Test", "Test", 100);
+        when(mockStorage.findReviewById(1L)).thenReturn(testReview);
+    }
+
+    @Test
+    public void displayReviewReturnsReviewTemplate() {
+        String result = underTest.displayReview(1L, model);
+        assertThat(result).isEqualTo("reviews");
+    }
+
+    @Test
+    public void displayReviewInteractsWithDependenciesCorrectly() {
+        underTest.displayReview(1L, model);
+        verify(mockStorage).findReviewById(1L);
+        verify(model).addAttribute("review", testReview);
+    }
+
+    @Test
+    public void displayReviewMappingIsCorrect() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/reviews/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("reviews"))
+                .andExpect(model().attributeExists("review"))
+                .andExpect(model().attribute("review", testReview));
+    }
+}
