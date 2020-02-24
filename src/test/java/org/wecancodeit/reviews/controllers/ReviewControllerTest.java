b16bcade3f46234fa35fe25ca0e6147a37848f78
Noah Doughty <noahdoughty@gmail.com>
2020-02-24 10:28:23 -0500

    Added forms
    Added hashtags and reviews forms.

--- src/test/java/org/wecancodeit/reviews/controllers/ReviewControllerTest.java
+++ src/test/java/org/wecancodeit/reviews/controllers/ReviewControllerTest.java
@@ -14,7 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class ReviewControllerTest {
+public class
+
+ReviewControllerTest {
 
     private ReviewController underTest;
     private Model model;
