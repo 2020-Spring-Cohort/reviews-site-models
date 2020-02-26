f4cd5f6265606b9747db142a01b1c0f4d791c823
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-26 11:06:28 -0500

    Dependencies
    Provided mock dependencies for the web application test
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/test/java/org/wecancodeit/reviews/integration/SpringWebAppTest.java
+++ src/test/java/org/wecancodeit/reviews/integration/SpringWebAppTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.wecancodeit.reviews.storage.CategoryStorage;
+import org.wecancodeit.reviews.storage.HashtagStorage;
+import org.wecancodeit.reviews.storage.ReviewStorage;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -16,6 +20,12 @@ public class SpringWebAppTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private ReviewStorage reviewStorage;
+    @MockBean
+    private CategoryStorage categoryStorage;
+    @MockBean
+    private HashtagStorage hashtagStorage;
 
     @Test
     public void shouldReceiveOKFromReviewsEndpoint() throws Exception {
