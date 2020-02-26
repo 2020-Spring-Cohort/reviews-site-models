53788c9c5e79b9feeb38f805e412b8796e0922ac
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-26 11:05:31 -0500

    Test Changes and Fixes
    Added DirtiesContext to tests that may affect other tests
    
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/test/java/org/wecancodeit/reviews/integration/SmokeTests.java
+++ src/test/java/org/wecancodeit/reviews/integration/SmokeTests.java
@@ -2,8 +2,10 @@ package org.wecancodeit.reviews.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext
 class SmokeTests {
 
     @Test
